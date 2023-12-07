5a9768b8114c3a1a35df24559b50c7270ba6e9c5
Rajee <rajeevan@gmail.com>
2023-12-07 21:51:09 +0530

    new change

--- index.js
+++ index.js
@@ -6,32 +6,38 @@ require('dotenv').config();//
 
 
 
-// Here, the code imports required Node.js modules. 
-// Express is used for creating the server, Mongoose for MongoDB database interaction, 
-// and routes to manage API routes.
-
-const express = require('express');
+// It initializes an Express application that will handle HTTP requests.
+const express = require('express'); 
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
+//It initializes an Express application that will handle HTTP requests.
 const app = express();
 
+//express.json() is middleware that parses incoming JSON requests.
 app.use(express.json());
+
+//sets up the API routes. All routes defined in the routes module will start with /api.
 app.use('/api', routes)
 
-const mongoString = process.env.DATABASE_URL;
 
+const mongoString = process.env.DATABASE_URL; //Retrieves the MongoDB connection URL from the environment variables.
+
+//connects to the MongoDB database using the URL
 mongoose.connect(mongoString);
+//holds the reference to the database connection.
 const database = mongoose.connection;
 
+ //listens for any errors that occur during the connection.
 database.on('error', (error) => {
     console.log(error)
 })
-
+//listens for a single successful connection.
 database.once('connected', () => {
     console.log('Database Connected');
 })
 
-
+//app.listen() starts the server on port 6002.
+//Once the server starts successfully, it logs a message confirming the server start with the specified port
 app.listen(6002, () => {
     console.log(`Server Started at ${6002}`)
 })
