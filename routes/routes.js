1ff8e62740c24cfeaba7701c8566846f81c9017c
Rajee <rajeevan@gmail.com>
2023-12-07 15:41:05 +0530

    nodes js -2

---
+++ routes/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router()
+
+module.exports = router;
+
+const Model = require('../model/model');
+
+//post method
+
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
